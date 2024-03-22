7748f4884e9d211ce3ce48c9c6f5ca2738ad913d
artem schegortzov <tema.chegortzov@mail.ru>
2024-03-22 13:03:52 +0300

    done

--- src/utils/sortArrayBy.js
+++
@@ -1,9 +0,0 @@
-function sortArrayBy(array, sortBy, reverse) {
-  reverse = reverse == 'false' ? false : true
-  //return new array sorted by sortBy
-  return [...array].sort((a, b) =>
-    a[sortBy] > b[sortBy] ? (reverse ? -1 : 1) : reverse ? 1 : -1
-  )
-}
-
-export default sortArrayBy
