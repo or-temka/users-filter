73b8c06686f1ba04702d89c548e6abfae8e32fe8
artem schegortzov <tema.chegortzov@mail.ru>
2024-03-22 13:35:20 +0300

    added hide reset filter btn

--- src/components/users-filter/ClearFiltersBtn.jsx
+++ src/components/users-filter/ClearFiltersBtn.jsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styles from './ClearFiltersBtn.module.css'
 
 function ClearFiltersBtn({ setSearchValue }) {
+  const locationParams = useLocation().search
+  console.log(locationParams)
+
   return (
-    <Link
-      className={styles.ClearFiltersBtn}
-      to=""
-      onClick={() => {
-        setSearchValue('')
-      }}
-    >
-      <div className={styles.ClearFiltersBtn__svg}></div>
-      <span className={styles.ClearFiltersBtn__text}>Очистить фильтр</span>
-    </Link>
+    <>
+      {locationParams && (
+        <Link
+          className={styles.ClearFiltersBtn}
+          to=""
+          onClick={() => {
+            setSearchValue('')
+          }}
+        >
+          <div className={styles.ClearFiltersBtn__svg}></div>
+          <span className={styles.ClearFiltersBtn__text}>Очистить фильтр</span>
+        </Link>
+      )}
+    </>
   )
 }
 
